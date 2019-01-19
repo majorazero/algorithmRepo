03db2dca435a3c76d3ac87d2ff39d3fb39651394
majorazero <daniel.cw.hsu@gmail.com>
2019-01-18 19:13:44 -0800

    this terrible clunky code"

---
+++ stringCompression.js
@@ -0,0 +1,49 @@
+
+let stringCompression = (string) => {
+  let compress = '';
+  let edited = false;
+  let arr = [];
+  let obj = {char: string[0], count: 0};
+  for(let i = 0; i < string.length; i++){
+    //if the char matches we count it
+    if(obj.char === string[i]){
+      obj.count++;
+      if(obj.count > 1){
+        edited = true;
+      }
+    }
+    //if it doesnt match
+    else{
+      //we push the object
+      arr.push(obj);
+      //instantiate a new object
+      if(string[i+1] !== undefined){
+        obj = {char: string[i+1], count: 0};
+      }
+    }
+    if(i === string.length-1){
+      if(obj.char === string[i]){
+        obj.count++;
+      }
+      else{
+        obj={char: string[i], count: 1};
+      }
+      arr.push(obj);
+    }
+  }
+
+  for(let i = 0; i < arr.length; i++){
+    compress += `${arr[i].char}${arr[i].count}`;
+  }
+  if(edited === false){
+    return string;
+  }
+  else{
+    return compress;
+  }
+}
+
+console.log(stringCompression('aabbaaabbbbb'));
+console.log(stringCompression('aabcccccaaac'));
+console.log(stringCompression(''));
+console.log(stringCompression('abcd'));
