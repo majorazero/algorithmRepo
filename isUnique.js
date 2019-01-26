130e927ce9ad74c96cb81bc890c39b5b73ce79ae
majorazero <daniel.cw.hsu@gmail.com>
2019-01-25 16:18:28 -0800

    added isUnique

---
+++ isUnique.js
@@ -0,0 +1,13 @@
+const isUnique = (str) => {
+  for(let i = 0; i < str.length; i++){
+    for(let j = i+1; j < str.length; j++){
+      if(str[i] === str[j]){
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(isUnique("hello"));
+console.log(isUnique("helo"));
